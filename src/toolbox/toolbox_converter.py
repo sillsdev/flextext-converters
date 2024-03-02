565b0496525bdbd6c065d0536cc2f4ff2ac303b4
Orion202021 <94141243+Orion202021@users.noreply.github.com>
2024-03-01 17:04:40 -0700

    setup starting project files, notes: requirements_dev.txt - combination of previous file and tutorial setup.py - very basic version
    tox is working
    added basic GH action to use tox (not tested yet)
    
    made .gitignore include .idea (pycharm) files

---
+++ src/toolbox/toolbox_converter.py
@@ -0,0 +1,19 @@
+def function():
+    return True
+
+
+def function2():
+    return False
+
+
+def throw_function():
+    raise ValueError()
+
+
+def main():
+    print("<<< Toolbox Converter >>>")
+    print("<<< Converter Termination >>>")
+
+
+if __name__ == "__main__":
+    main()
