7a5bbe9f5d46e755df982e6f2548ca3336097010
Major-Q <63882792+Major-Q@users.noreply.github.com>
2024-03-04 17:53:46 -0700

    Add toolbox file reader & parser

---
+++ tests/test_toolbox_operations.py
@@ -0,0 +1,12 @@
+import pytest
+
+from toolbox.toolbox_operations import toolbox_file_reader, toolbox_data_parser
+
+
+def test_toolbox_file_reader():
+    assert toolbox_file_reader("")
+
+
+def test_toolbox_data_parser():
+    assert toolbox_data_parser("")
+
