94c05543703502175c4f5b1ca782714779c73f5b
Major-Q <63882792+Major-Q@users.noreply.github.com>
2024-03-05 14:46:38 -0700

    Reformat the toolbox parsing method
    File reader and parsing method tested

--- src/toolbox/toolbox_operations.py
+++ src/toolbox/toolbox_operations.py
@@ -5,48 +5,34 @@
 
     Project: Toolbox to Fieldworks Text Migration (LightSys)
 
-    Description:
+    File: toolbox_operations.py
 
+    Description:
+        File has two methods one for reading a toolbox file and one for parsing the read data
 
     Modification log:
         3-4-24:
             Add toolbox file reader method
             Add toolbox parsing method
+        3-5-24:
+            Reformat the toolbox parsing method
 
 """
 
 
 def toolbox_file_reader(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         toolbox_data = f.read()
 
     return toolbox_data
 
-# toolbox_data = "\_sh v3.0  621  Text
-#
-# \id Freeform
-# \ref
-# \tx This has a translation.
-# \ft It is a freeform translation.
-# \nt"
-
 
 def toolbox_data_parser(toolbox_data):
-    m_dict = {}                                     # marker dictionary
-    marker = ""
-    m_data = ""                                     # marker data
-    count = 0
-    for i, c in enumerate(toolbox_data):
-        if c == '\\':                               # get marker
-            if count != 0: m_dict[marker] = m_data  # add item to dictionary
-            c2 = ""
-            while c2 != ' ':
-                c2 = enumerate(toolbox_data)
-                marker += c2
+    m_dict = {}  # marker dictionary
+    lines = toolbox_data.split("\n")  # list of each line in the toolbox_data
+    for line in lines:
+        marker, sep, m_data = line.partition(" ")  # get marker and marker data
+        m_dict[marker] = m_data  # add item to dictionary
 
-            m_data = ""
-            count += 1
-        else:                                       # get marker data
-            m_data += c
-
+    del m_dict[""]  # dictionary has an extra item, this line deletes it
     return m_dict
