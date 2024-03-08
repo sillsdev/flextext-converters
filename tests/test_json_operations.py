8d7e678133435c4742e5d708ce5ea0c3386d46c8
Gavin Montgomery <gavinmontgomery@cedarville.edu>
2024-03-08 10:00:09 -0700

    Update json_operation method to use file paths Update test_json_operations accordingly

--- tests/test_json_operations.py
+++ tests/test_json_operations.py
@@ -24,16 +24,18 @@ test_map2 = {"\\ref": sub_test_map2}
 
 
 def test_json_creation():
-    output_path = "./src/toolbox/json_marker_files/test_json_creation.json"
-    save_map_to_json(test_map1, "test_json_creation")
+    output_path = "./tests/output_test_files/test_json_creation.json"
+    save_map_to_json(test_map1, output_path)
     assert os.path.isfile(output_path) is True
 
 
 def test_json_contents_simple():
-    save_map_to_json(test_map1, "test_json_contents_simple")
-    assert (test_map1 == load_map_from_json("test_json_contents_simple")) is True
+    output_path = "./tests/output_test_files/test_json_contents_simple.json"
+    save_map_to_json(test_map1, output_path)
+    assert (test_map1 == load_map_from_json(output_path)) is True
 
 
 def test_json_contents_complex():
-    save_map_to_json(test_map2, "test_json_contents_complex")
-    assert (test_map2 == load_map_from_json("test_json_contents_complex")) is True
+    output_path = "./tests/output_test_files/test_json_contents_complex.json"
+    save_map_to_json(test_map2, output_path)
+    assert (test_map2 == load_map_from_json(output_path)) is True
